36eba71db670dab2ab5185de95fd82d350042a73
jazmin.bejarano@maggioli-latam.com <jazmin.bejarano@maggioli-latam.com>
2023-04-28 18:29:01 -0500

    Layers controls

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,53 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { latLng, Map, tileLayer, geoJSON, Control } from 'leaflet';
-
+import { latLng, Map, tileLayer, geoJSON, Control, layerGroup } from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
+
 export class MapComponent {
-  // Instanciar el Mapa
-  public map: Map | undefined;
+
+  control = new Control.Layers();
 
   constructor(private http: HttpClient) {
-  }
-
-  //Mapa base 
-  LAYER_OSM = {
-    id: 'openstreetmap',
-    name: 'Open Street Map',
-    enabled: false,
-    layer: tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    /* layer: tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', */
-    /* layer: tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager_nolabels/{z}/{x}/{y}.png,', */
-    /* layer: tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', */
-    /* layer: tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', */
-    {
-      maxZoom: 18,
-      attribution: 'Open Street Map'
-    })
   };
 
+  // Base Layer
   options = {
-    layers: [this.LAYER_OSM.layer],
+    layers: [tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 })],
     zoom: 12,
     center: latLng(4.66, -74.1)
   };
 
   onMapReady(map: Map) {
-    this.map = map;
-
-    this.http.get('./assets/geojson/map.geojson').subscribe((json: any) => {
-      geoJSON(json).addTo(map);
+    // Base layer controls
+    this.http.get('./assets/geojson/maps.geojson').subscribe((maps: any) => {
+      for (let map of maps) {
+        this.control.addBaseLayer(tileLayer(map.url, { maxZoom: map.maxZoom }), map.title)
+      }
     });
 
-    this.http.get('./assets/geojson/map_1.geojson').subscribe((json: any) => {
-      geoJSON(json).addTo(map);
+    // Overlay controls
+    this.http.get('./assets/geojson/circles.geojson').subscribe((json: any) => {
+      this.control.addOverlay(geoJSON(json).addTo(layerGroup()), json.title);
     });
+    this.http.get('./assets/geojson/squares.geojson').subscribe((json: any) => {
+      this.control.addOverlay(geoJSON(json).addTo(layerGroup()), json.title);
+    });
+
+    // Add controls
+    this.control.addTo(map);
   }
-
 
 }
